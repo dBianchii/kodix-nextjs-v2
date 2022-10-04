be22100351b2915decce0602b7e2e685d74373c0
Gabriel <gdbianchii@gmail.com>
2022-10-04 00:00:50 -0300

    Fixed bug with pageProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 
 import { extendTheme } from '@chakra-ui/react'
+import { Session } from 'next-auth'
 
 const colors = {
 	brand: {
@@ -23,7 +24,7 @@ const fonts = {
 
 const theme = extendTheme({ colors, config, fonts} )
 
-function MyApp({Component, pageProps: { session, ...pageProps }}: AppProps) {
+function MyApp({Component, pageProps,}: AppProps<{ session: Session}>) {
   return (
     <ChakraProvider theme={theme}>
 		<SessionProvider session={pageProps.session}>
